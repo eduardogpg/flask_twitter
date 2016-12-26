a7b650d34ac09d1f9e5bf9c34d51715187992570
Eduardo Ismael <eduardo78d>
2016-12-25 18:29:49 -0600

    TDD user login

--- app/models.py
+++ app/models.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from . import db
 import datetime
+from sqlalchemy import or_
+from sqlalchemy.exc import InvalidRequestError
+
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
+
+"""
+User.query.filter_by(id=123).delete() or User.query.filter(User.id == 123).delete()
+"""
 
 class User(db.Model):
 	__tablename__ = 'users'
@@ -12,12 +24,16 @@ class User(db.Model):
 
 	def __init__(self, username, password, email):
 		self.username = username
-		self.encrypted_password = password
+		self.password = password
 		self.email = email
-	
+		
 	@property
 	def password(self):
 		return self.encrypted_password
+	
+	@password.setter
+	def password(self, password):
+		self.encrypted_password = generate_password_hash(password)
 
 	@staticmethod
 	def new(username = '', password = '', email = ''):
@@ -28,18 +44,70 @@ class User(db.Model):
 		user = User.new(username, password, email)
 		if user.save():
 			return user
-			
+		return user.errors #Raise error?
+		
 	@staticmethod
-	def find(user_id):
-		return User.query.get(User.id == user_id)
+	def find(id):
+		return User.query.get(id)
+
+	@staticmethod
+	def authenticated(identifier, password):
+		user = User.find_by_identifier(identifier)
+		if user is not None:
+			return user.verify_password(password)
+
+	@classmethod
+	def find_by_identifier(cls, identifier):
+		conditional = or_(User.username == identifier, User.email == identifier)
+		return User.query.filter(conditional).first()
+
+	@classmethod
+	def select_all(cls):
+		pass
 
 	@classmethod
 	def delete_all(cls):
 		db.session.query(User).delete()
 		db.session.commit()
 
+	def verify_password(self, password):
+		return check_password_hash(self.encrypted_password, password)
+
 	def save(self):
-		db.session.add(self)
-		db.session.commit()
-		return True
+		if self.is_valid_to_save():
+			db.session.add(self)
+			db.session.commit()
+			return True
+
+	def destroy(self):
+		try:
+			db.session.delete(self)
+			return True
+		except InvalidRequestError:
+			return False
+
+	def is_valid_to_save(self):
+		self.errors = {}
+		if not self.is_valid_email():
+			self.errors['email'] = 'El email no es correcto.'
+		if not self.is_valid_username_len():
+			self.errors['username'] = 'Maximo de 50 caracteres.'
+
+		if not self.errors:
+			return True
+	
+	def is_valid_email(self):
+		import re
+		return re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", self.email)
+
+	def is_valid_username_len(self):
+		return not len(self.username) > 50
+
+	def is_validate_username(self):
+		return ' ' not in self.username
+
+	def __str__(self):
+		return self.username
+
+
 
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,31 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import unittest
 from app import create_app, db
 from app.models import User
-
+#http://docs.python-guide.org/en/latest/writing/tests/
 
 class UserTestCase(unittest.TestCase):
 	def setUp(self):
 		self.app = create_app('testing')
 		self.app_context = self.app.app_context()
 		self.app_context.push()
-		db.create_all()
+		self.params = self.get_params()
+		self.invalid_params = self.get_invalid_params()
+		self.create_table()
 		
 	def tearDown(self):
-		db.drop_all()
 		self.app_context.pop()
 
 	def test_new_user(self):
-		username = 'eduardo'
-		password = 'password'
-		email = 'eduardo78d@gmail.com'
+		user = self.new_user(self.params) 
+		self.assertTrue(user is not None)
 
-		self.user = User.new(username, password, email)
-		response = self.user.username == username and self.user.password == password and self.user.email == email
-		self.assertTrue(response)
+	def test_validate_email_user(self):
+		user = self.new_user(self.params)
+		user.email = 'invalid email'
+		self.assertFalse(user.is_valid_email())
+
+	def test_validate_len_username_user(self):
+		user = self.new_user(self.params)
+		user.username = 'eduardo'*50
+		self.assertFalse(user.is_valid_username_len())
+
+	def test_validate_username(self):
+		user = self.new_user(self.params)
+		user.username = 'eduardo ismael'
+		self.assertFalse(user.is_validate_username())
 
 	def test_save_user(self):
-		response = self.user.save() and self.user.id is not None and self.user.id > 0
+		user = self.new_user(self.params)
+		user.save()
+		self.assertTrue( user is not None and user.id > 0)
+
+	def test_save_user_invalid_email(self):
+		user = self.new_user(self.params)
+		user.email = 'invalid email'
+		user.save()
+		email_error = user.errors.get('email', '')
+		self.assertEqual(email_error, 'El email no es correcto.')
+
+	def test_save_user_invalid_username(self):
+		user = self.new_user(self.params)
+		user.username = 'eduardo'*50
+		user.save()
+		username_error = user.errors.get('username', '')
+		self.assertEqual(username_error, 'Maximo de 50 caracteres.')
+
+	def test_create_user(self):
+		user = self.create_user(self.params)
+		self.assertTrue(user.id > 0)
+
+	def test_create_user_with_invalid_params(self):#Debemos de arreglar esta prueba de aceptación
+		response = self.create_user(self.invalid_params)
+		self.assertIsInstance(response, dict)
+		
+	def test_find_user(self):
+		self.create_user(self.params)
+		user = User.find(1)
+		response = user.username is not None and user.id == 1 and user.username == self.params['username']
 		self.assertTrue(response)
 
+	def test_update_username_user(self):
+		self.create_user(self.params)
+		user = User.find(1)
+		new_username = 'new username'
+		user.username = new_username
+		user.save()
+		user = User.find(1)
+		self.assertTrue(user.username == new_username)
+
+	def test_encrypted_password(self):
+		user = self.create_user(self.params)
+		self.assertTrue( user.password != self.params['password'] and len(user.password) == 66)
+
+	def test_find_user_by_username(self):
+		self.create_user(self.params)
+		user = User.find_by_identifier(self.params['username'])
+		self.assertEqual(user.id, 1)
+	
+	def test_find_user_by_email(self):
+		self.create_user(self.params)
+		user = User.find_by_identifier(self.params['email'])
+		self.assertEqual(user.id, 1)
+
+	def test_login_user(self):
+		self.create_user(self.params)
+		user = User.authenticated(self.params['email'], self.params['password'])
+		self.assertTrue(user is not None)
+
+	def create_table(self):
+		db.drop_all()
+		db.create_all()
+	
+	def create_user(self, params):
+		return User.create(params['username'], params['password'], params['email'])
+
+	def new_user(self, params):
+		return User.new(params['username'], params['password'], params['email'])
+
+	def get_params(self):
+		return {
+				'username' : 'eduardo78d',
+				'password' : 'password',
+				'email' : 'eduardo78d@gmail.com'
+		}
+
+	def get_invalid_params(self):
+		return {
+				'username' : 'eduardo78d' * 10,
+				'password' : 'password',
+				'email' : 'eduardo78dgmailcom'
+		}
 
 
